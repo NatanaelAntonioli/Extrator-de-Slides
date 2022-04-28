e5f31c5ddb1789e428c49cca49bf1aca4b22a8c0
tamireinhorn <tamtamireinhorn@gmail.com>
2022-04-28 05:10:23 -0300

    Flake8

--- src/ExtratorSlides/SlideExtractor.py
+++ src/ExtratorSlides/SlideExtractor.py
@@ -1,9 +1,9 @@
-# Rewriting code for simplicity's sake. 
+# Rewriting code for simplicity's sake.
 import mimetypes
-from multiprocessing.sharedctypes import Value
 from tkinter import filedialog as fd
-import cv2 
+import cv2
 from skimage.metrics import structural_similarity
+
 
 def receive_input() -> int:
     """Asks the user for the minimal time, in seconds, that makes a slide relevant. Only accepts positive numbers.
@@ -17,11 +17,11 @@ def receive_input() -> int:
     raw_seconds = input("Quanto deve durar um slide (em segundos) para que seja relevante? ")
     try:
         seconds = int(raw_seconds)
-    except:
+    except ValueError:
         raise ValueError("Valor inválido. Segundos são um número inteiro maior que 0.")
     if seconds <= 0:
         raise ValueError("Valor inválido. Segundos são um número inteiro maior que 0.")
-    return seconds 
+    return seconds
 
 
 def choose_video() -> str:
@@ -33,12 +33,13 @@ def choose_video() -> str:
     Returns:
         str: The full path to the file chosen by the user.
     """
-    filename: str = fd.askopenfilename(title= 'Escolha um arquivo de vídeo.')
-    #mypy can't see the library where this is properly defined
+    filename: str = fd.askopenfilename(title= 'Escolha um arquivo de vídeo.')  # noqa: E251
+    # mypy can't see the library where this is properly defined
     # Verifies for valid video file
-    if not mimetypes.guess_type(filename)[0].startswith('video'): # type: ignore[union-attr]
+    if not mimetypes.guess_type(filename)[0].startswith('video'):  # type: ignore[union-attr]
         raise ValueError("O programa apenas suporta arquivos de vídeo. Escolha um arquivo de vídeo.")
     return filename
+
 
 def calculate_length_video(video: cv2.VideoCapture) -> int:
     """Calculates the approximate length, in minutes, of a given video file.
@@ -51,12 +52,12 @@ def calculate_length_video(video: cv2.VideoCapture) -> int:
     """
     fps = video.get(cv2.CAP_PROP_FPS)      #
     frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-    duration =  int(frame_count / fps)
-    return duration 
+    duration = int(frame_count / fps)
+    return duration
 
 
 def calculate_iterations(video_length: int, seconds: int) -> int:
-    """Calculate the number of loops to be used in the processing of the video. 
+    """Calculate the number of loops to be used in the processing of the video.
 
     Args:
         video_length (int): Size of video in seconds, calculated in calculate_length_video().
@@ -66,11 +67,11 @@ def calculate_iterations(video_length: int, seconds: int) -> int:
         ValueError: If the quantity of seconds is superior to the length of the video in itself.
 
     Returns:
-        int: Returns the number of times the processing and comparing of frames will be done. 
+        int: Returns the number of times the processing and comparing of frames will be done.
     """
     if video_length < seconds:
         raise ValueError(f"Você escolheu {seconds} para um slide ser relevante, mas o vídeo dura menos {video_length} segundos")
-    return (video_length // seconds) + 1 
+    return (video_length // seconds) + 1
 
 
 def image_comparison(imageA: cv2.VideoCapture, imageB: cv2.VideoCapture, threshold: float = 0.9) -> bool:
@@ -81,7 +82,7 @@ def image_comparison(imageA: cv2.VideoCapture, imageB: cv2.VideoCapture, thresho
         imageA (cv2.VideoCapture): First frame for comparison
         imageB (cv2.VideoCapture): Second frame for comparison
         threshold (float, optional): Minimal score to define that images are different. Defaults to 0.9.
-    
+
     Raises:
         ValueError: If the threshold is outside the range of (0,1). If minimal score is 1, even the same image is different from itself.
         If the minimal score is 0, no image is different enough.
@@ -91,15 +92,7 @@ def image_comparison(imageA: cv2.VideoCapture, imageB: cv2.VideoCapture, thresho
     """
     if threshold >= 1 or threshold <= 0:
         raise ValueError("A tolerância para similaridade tem que ser um número positivo entre 0 e 1.")
-    grayA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY) 
+    grayA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
     grayB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
-    score: float = structural_similarity(grayA, grayB) # The closer to 1 in absolute, the more similar they are. 0.9 is my arbitrary threshold.
-    return abs(score) < threshold # If the similarity is not that close to 1, they're different. 
-
-# def process_video() -> bool:
-#     seconds = receive_input()
-#     video_file = choose_video()
-#     capture = cv2.VideoCapture(video_file)
-#     video_length = calculate_length_video(capture)
-
-#     pass
+    score: float = structural_similarity(grayA, grayB)  # The closer to 1 in absolute, the more similar they are. 0.9 is my arbitrary threshold.
+    return abs(score) < threshold  # If the similarity is not that close to 1, they're different.
