67d1cb2f6ced45a8370ce3c96358a8d579d21e07
Tamir <37386851+tamireinhorn@users.noreply.github.com>
2023-02-25 02:56:12 -0300

    Add typing session

--- noxfile.py
+++ noxfile.py
@@ -5,3 +5,9 @@ import nox
 def lint(session):
     session.install('flake8')
     session.run('flake8', 'src/ExtratorSlides/SlideExtractor.py')
+
+@nox.session
+
+def typing(session):
+    session.install('mypy')
+    session.run('mypy', 'src/ExtratorSlides/SlideExtractor.py')
